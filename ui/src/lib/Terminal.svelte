0e8f5e61b343c5d73deb340f325ce3f8b8db2d05
archemich <archemich@gmail.com>
2024-04-12 12:19:13 +0300

    feat: add ui barebone

---
+++ ui/src/lib/Terminal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { Terminal } from '@xterm/xterm';
+    
+    const terminal = new Terminal();
+    let prompt = '';
+     
+    function openTerminal(node: Element){
+        terminal.open(node);
+    }
+
+    function writeTerminal(e: Event) {
+        terminal.write(prompt);
+        prompt="";
+    }
+    
+</script>
+
+<div class="termianl">
+    <form on:submit|preventDefault={writeTerminal}>
+        <input bind:value={prompt} type="text" name="prompt"/>
+    </form>
+    <div use:openTerminal></div>
+
+</div>
